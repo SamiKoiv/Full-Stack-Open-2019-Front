df2e2c1330f4f26056ff9a5caa1f22adc51c49c5
sekopallo <sekopallo@gmail.com>
2019-10-14 11:03:37 +0300

    puhelinluettelo step6

--- Osa_2/puhelinluettelo/src/App.js
+++ Osa_2/puhelinluettelo/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import NewContactForm from './components/NewContactForm'
 import Contacts from './components/Contacts'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
   const [searchHits, setSearchHits] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('resolved')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
